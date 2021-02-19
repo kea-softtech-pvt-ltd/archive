d864776f6fcec20bf8c2dc68b54d68045e273fc5
unknown <gauravp1@keasofftech.com>
2021-02-19 15:08:32 +0530

    tbl name in changes on that file

--- common/includes/tbl-names.php
+++ common/includes/tbl-names.php
@@ -5,4 +5,7 @@ define("PROPERTY", 'property');
 define("ADMIN", 'admin');
 define("AMENITIES", 'amenities');
 define("UNITS", 'units');
+define("CITY", 'city');
+define("STATE", 'state');
+define("COUNTRY", 'country');
 ?>
