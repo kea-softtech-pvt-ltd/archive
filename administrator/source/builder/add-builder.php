1908bf9c1ae96556dc0aac0fcc52210524b11569
unknown <gauravp1@keasofftech.com>
2021-02-19 15:01:11 +0530

    changes on image in add-builder.php file

--- administrator/source/builder/add-builder.php
+++ administrator/source/builder/add-builder.php
@@ -1,6 +1,7 @@
 <?php
 	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . '/includefiles.php');
 	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . "/common/model/builder.php");
+	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . "/common/model/city.php");
 
 if (isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '')  
 {
@@ -35,8 +36,10 @@ if (isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '')
 
 	//print_r($_REQUEST); 
 	// fields -> id	name	partner_name	pan	telephone	email	avatar	city	street_address	state	zip	properties	create_at	modified_at	status
+	$builderObj = new Model_Builders();
+	$cityObj = new Model_City();
 	if(isset($_POST['builderSave'])) {
-		$builderObj = new Model_Builders();
+		
 		//echo "submitted";
 		$builderArray['name'] = $_POST['name'];
 		$builderArray['partner_name'] = $_POST['partner_name'];
@@ -59,8 +62,22 @@ if (isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '')
 		$builderId = $builderObj->addBuilderByValue($builderArray);
 		header("Location: " . SITE_URL . "/admin/builder");
 	}
-
+	if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] != ''){	
+	$citiesListArray = $builderObj->getCities();
+	$smarty->assign('citiesListArray', $citiesListArray);
+	$statesListArray = $builderObj->getStates();
+	$smarty->assign('statesListArray', $statesListArray);
 	$smarty->assign('moduleName', 'Add builder');
 	//echo ADMIN_TEMPLATEDIR;
 	$smarty->display(ADMIN_TEMPLATEDIR . '/builder/add-builder.tpl');
+	}else{
+		header("Location: " . SITE_URL . "/admin/login");
+	}
+	
+
+	// $citiesListArray = $builderObj->getCities();
+	// $smarty->assign('citiesListArray', $citiesListArray);
+	// $smarty->assign('moduleName', 'Add builder');
+	// //echo ADMIN_TEMPLATEDIR;
+	// $smarty->display(ADMIN_TEMPLATEDIR . '/builder/add-builder.tpl');
 ?>
