8e9d02261ffc06d4561c470993571089ed1e0383
unknown <gauravp1@keasofftech.com>
2021-02-20 15:14:48 +0530

    remove image add font awesome

--- administrator/source/amenities/add-amenities.php
+++ administrator/source/amenities/add-amenities.php
@@ -4,42 +4,50 @@
 
  $msg=array();
  $msg1=array();
+ $msg2=array();
 
 if(isset($_POST['amenitiesSave']))
  {
 
-	$targetDir = "../upload/amenities/"; 
-	$imagearray = array();
-	foreach($_FILES['image']['name'] as $key=>$val){ 
-	   // File upload path 
-		$fileName =  time().'_'.basename($_FILES['image']['name'][$key]); 
-		$targetFilePath = $targetDir . $fileName; 
+// 	$targetDir = "../upload/amenities/"; 
+// 	$imagearray = array();
+// 	foreach($_FILES['image']['name'] as $key=>$val){ 
+// 	   // File upload path 
+// 		$fileName =  time().'_'.basename($_FILES['image']['name'][$key]); 
+// 		$targetFilePath = $targetDir . $fileName; 
 		
-		// Check whether file type is valid 
-	   $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
-		if(move_uploaded_file($_FILES["image"]["tmp_name"][$key], $targetFilePath)){ 
-			array_push($imagearray,$fileName);
-		}
-	} 
+// 		// Check whether file type is valid 
+// 	   $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
+// 		if(move_uploaded_file($_FILES["image"]["tmp_name"][$key], $targetFilePath)){ 
+// 			array_push($imagearray,$fileName);
+// 		}
+// 	} 
    
-$imagelist = implode(",",$imagearray);
+// $imagelist = implode(",",$imagearray);
 
 
 
-	if (trim($_POST['name'])== '' || trim($imagelist)==''){
+	if (trim($_POST['name'])== '' /* || trim($imagelist)==''*/)
+	{
 		if (trim($_POST['name'])== '') {
-		$msg = "user name cant be emty.";
+		$msg = "Amenities name cant be emty.";
 		$smarty->assign('message', $msg);
 	   }
-	   if (trim($imagelist)== '') {
-		$msg1 = "Image cant be emty.";
-		$smarty->assign('message1', $msg1);
+	   if (trim($_POST['font_awaesome'])== '') {
+		$msg2 = "Font awaesome cant be emty.";
+		$smarty->assign('message2', $msg2);
+	   }
+	//    if (trim($imagelist)== '')
+	//     {
+	// 	$msg1 = "Image cant be emty.";
+	// 	$smarty->assign('message1', $msg1);
 		
-	}
+	// 	}
     }
 	else {
 		$amenitiesObj = new Model_Amenities();
 		$amenitiesArray['name'] = $_POST['name'];
+		$amenitiesArray['font_awaesome'] = $_POST['font_awaesome'];
 		$amenitiesArray['images'] = $imagelist; // multiple
 		$amenitiesArray['created_at'] = date('Y/m/d H:i:s');
 		$amenitiesArray['modified_at'] = date('Y/m/d H:i:s');
