ad130eda9bdb3b564cf0e7dbfe58030af844cc70
unknown <gauravp1@keasofftech.com>
2021-02-20 15:25:50 +0530

    changes on ui

--- administrator/source/builder/add-builder.php
+++ administrator/source/builder/add-builder.php
@@ -3,7 +3,7 @@
 	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . "/common/model/builder.php");
 	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . "/common/model/city.php");
 
-if (isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '')  
+	if (isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '')  
 {
 	$filename = time().'_'.$_FILES["image"]["name"];
 	$tempname = $_FILES["image"]["tmp_name"];
@@ -12,52 +12,25 @@ if (isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '')
 	$_POST['image'] = $filename;
 }
 
-// multiple image 
-
-// if (isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '') {
-// 	$targetDir = "../upload/"; 
-// 	$imagearray = array();
-// 	foreach($_FILES['image']['name'] as $key=>$val){ 
-// 		// File upload path 
-// 		$fileName =  time().'_'.basename($_FILES['image']['name'][$key]); 
-// 		$targetFilePath = $targetDir . $fileName; 
-		 
-// 		// Check whether file type is valid 
-// 		$fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
-// 		if(move_uploaded_file($_FILES["image"]["tmp_name"][$key], $targetFilePath)){ 
-// 			array_push($imagearray,$fileName);
-// 		}
-// 	} 
-	
-// $imagelist = implode(",",$imagearray);
-// }
-
-// emd image
-
-	//print_r($_REQUEST); 
-	// fields -> id	name	partner_name	pan	telephone	email	avatar	city	street_address	state	zip	properties	create_at	modified_at	status
 	$builderObj = new Model_Builders();
 	$cityObj = new Model_City();
 	if(isset($_POST['builderSave'])) {
 		
-		//echo "submitted";
 		$builderArray['name'] = $_POST['name'];
-		$builderArray['partner_name'] = $_POST['partner_name'];
+		$builderArray['avatar'] = $_POST['image'];
 		$builderArray['telephone'] = $_POST['contact_number'];
 		$builderArray['email'] = $_POST['email'];
-		$builderArray['pan'] = $_POST['pan_number'];
+		$builderArray['register_number'] = rand(6,9999);
 		$builderArray['city'] = $_POST['city'];
+	//	$builderArray['m_con'] = implode(',',$_POST['m_contact']) ; its use for multiple add contact
 		$builderArray['street_address'] = $_POST['address'];
 		$builderArray['state'] = $_POST['state'];
 		$builderArray['zip'] = $_POST['zipcode'];
-		$builderArray['avatar'] = $_POST['image'];
-		//$builderArray['avatar'] = $imagelist; // multiple
 		$builderArray['create_at'] = date('Y/m/d H:i:s');
 		$builderArray['modified_at'] = date('Y/m/d H:i:s');
 		$builderArray['status'] = 1;
-		$builderArray['user_name'] = $_POST['name']; // user name add in name
+		$builderArray['user_name'] = $_POST['name'];
 		$builderArray['password'] = rand(6,9999);
-		//$builderArray['avatar'] = '';
 		$builderArray['properties'] = '';
 		$builderId = $builderObj->addBuilderByValue($builderArray);
 		header("Location: " . SITE_URL . "/admin/builder");
@@ -68,16 +41,9 @@ if (isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '')
 	$statesListArray = $builderObj->getStates();
 	$smarty->assign('statesListArray', $statesListArray);
 	$smarty->assign('moduleName', 'Add builder');
-	//echo ADMIN_TEMPLATEDIR;
 	$smarty->display(ADMIN_TEMPLATEDIR . '/builder/add-builder.tpl');
 	}else{
 		header("Location: " . SITE_URL . "/admin/login");
 	}
 	
-
-	// $citiesListArray = $builderObj->getCities();
-	// $smarty->assign('citiesListArray', $citiesListArray);
-	// $smarty->assign('moduleName', 'Add builder');
-	// //echo ADMIN_TEMPLATEDIR;
-	// $smarty->display(ADMIN_TEMPLATEDIR . '/builder/add-builder.tpl');
 ?>
