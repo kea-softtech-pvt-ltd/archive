8e45edff936d1bd0429f4b4225cd78abe7a1c033
unknown <gauravp1@keasofftech.com>
2021-03-05 11:01:17 +0530

    get all properties funtion use and add properties funtion use

--- common/model/properties.php
+++ common/model/properties.php
@@ -7,8 +7,7 @@
             parent::__construct();
     
         }	
-        
-        
+         
         ## Add builder in database
         function addPropertiesByValue($Array) {
             $this->InsertData( $this->property , $Array );		
@@ -16,19 +15,27 @@
             return $insertId;
         }
 
-        ## Get all builder details
-        function getAllBuilders($search='', $limit='',$offset='') {
+        ## Get all Properties details
+        function getAllProperties($search='', $limit='',$offset='') {
             $fields=array();	
             $tables=array($this->property);
             $where = array(" status = '1'");
             if($search != '') {
-                //$where[] = "concat(first_name,' ',last_name) like '%".$search."%'";
                 $where[] = "(concat(first_name,' ',last_name) LIKE '%".$search."%' OR email LIKE '%".$search."%' )";
             }
                 
             $result1 = $this->SelectData($fields,$tables, $where, $order = array(), $group=array(), $limit,$offset,0);
             $result= $this->FetchAll($result1); 
             return $result;		
+        }
+        public function getWing()
+        {
+            $fields=array('name','id','status');	//fetch fromdb
+            $tables=array('wing');
+            $where = array(" status = '1'");	
+            $result1 = $this->SelectData($fields,$tables, $where, $order = array(), $group=array(),$limit = "",0,0); 
+            $result= $this->FetchAll($result1);
+            return $result;	
         }
         
     }
