52788928f3806b7518fcad1d24dcf023b73b1ac1
unknown <gauravp1@keasofftech.com>
2021-03-08 19:13:52 +0530

    wing and floor database table defiednd

--- common/includes/tbl-names.php
+++ common/includes/tbl-names.php
@@ -8,4 +8,10 @@ define("UNITS", 'units');
 define("CITY", 'city');
 define("STATE", 'state');
 define("COUNTRY", 'country');
+define("BUILDING_TYPE", 'building_type');
+define("WING", 'wing');
+define("NEIGHBOURHOODS", 'neighbourhoods');
+define("FLOOR", 'floor');
+define("BANNER", 'banner');
+
 ?>
