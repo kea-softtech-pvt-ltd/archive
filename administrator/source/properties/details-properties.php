6138dbce4181979896452f9a987bc3642fbaffe9
unknown <gauravp1@keasofftech.com>
2021-03-10 12:23:35 +0530

    show table format properties

---
+++ administrator/source/properties/details-properties.php
@@ -0,0 +1,11 @@
+<?php
+	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . '/includefiles.php');
+	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . "/common/model/properties.php");
+
+	$propertiesObj = new Model_Property();
+	$propertiesListArray = $propertiesObj->getAllProperties();
+
+	$smarty->assign('propertiesListArray', $propertiesListArray);
+	$smarty->assign('moduleName', 'List of properties');
+	$smarty->display(ADMIN_TEMPLATEDIR . '/properties/details-properties.tpl');
+?>
