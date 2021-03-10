48b01a3d4156e7f6d48f9f3420f9c0f0c0a6f774
unknown <gauravp1@keasofftech.com>
2021-03-10 12:28:43 +0530

    that page use property ajax function add,edit view delete

--- administrator/ajax/ajax_properties.php
+++ administrator/ajax/ajax_properties.php
@@ -5,12 +5,22 @@
      $builderObj = new Model_Property();
 if($_REQUEST['action']=="addProperties")
 {
+
+	
     $propertyArray['name'] = $_POST['name'];
     $propertyArray['builder_name'] = $_POST['builder_name'];
     $propertyArray['started_date'] = $_POST['started_date'];
     $propertyArray['possession_date'] = $_POST['possession_date'];
     $propertyArray['rera_number'] = $_POST['rera_number'];
-    $result = $builderObj->addPropertiesByValue($propertyArray);
+
+	if(isset($_POST['propertyID']) && !empty($_POST['propertyID'])){
+		$builderObj->editUserValueById($propertyArray,$_POST['propertyID']);
+		$result = $_POST['propertyID'];
+	}else{
+		$result = $builderObj->addPropertiesByValue($propertyArray);
+	}
+
+    
 	if($result)
 
 	{	
@@ -42,7 +52,100 @@ if($_REQUEST['action']=="addPropertieAddress")
     $propertyArray['address'] = $_POST['address'];
     $propertyArray['description'] = $_POST['description'];
     $propertyArray['p_id'] = $_POST['propertyID'];
-    $result = $builderObj->addPropertieAddressByValue($propertyArray);
+	$result = $builderObj->addPropertieAddressByValue($propertyArray);
+
+
+	if($result)
+	{	
+		$response['status'] = '1'; 
+
+		$response['message'] = 'success';
+
+	}
+
+	else
+
+	{
+
+		$response['status'] = '0'; 
+
+		$response['message'] = 'error';
+
+	}
+	echo json_encode($response);
+}
+if($_REQUEST['action']=="addWing")
+{
+
+	for($i=0; $i<count($_POST['wings']);$i++)
+	{
+	$propertyArray['p_id'] = $_POST['propertyID'];
+    $propertyArray['name'] = $_POST['wings'][$i];
+	$propertyArray['totalFloor'] = $_POST['totalFloor'][$i];
+    $result = $builderObj->addWingsByValue($propertyArray);
+	}
+	if($result)
+	{	
+		$response['status'] = '1'; 
+
+		$response['message'] = 'success';
+
+	}
+
+	else
+
+	{
+
+		$response['status'] = '0'; 
+
+		$response['message'] = 'error';
+
+	}
+	echo json_encode($response);
+}
+if($_REQUEST['action']=="addFloor")
+{
+	for($i=0; $i<count($_POST['floor']);$i++)
+	{
+	$propertyArray['p_id'] = $_POST['propertyID'];
+	$propertyArray['wing'] = $_POST['wing'][$i];
+    $propertyArray['flat'] = $_POST['flat'][$i];
+	$propertyArray['floor'] = $_POST['floor'][$i];
+	$propertyArray['specality'] = $_POST['specality'][$i];
+    $result = $builderObj->addFloorsByValue($propertyArray);
+	}
+	if($result)
+	{	
+		$response['status'] = '1'; 
+
+		$response['message'] = 'success';
+
+	}
+
+	else
+
+	{
+
+		$response['status'] = '0'; 
+
+		$response['message'] = 'error';
+
+	}
+	echo json_encode($response);
+}
+if($_REQUEST['action']=="addUnits")
+{
+	for($i=0; $i<count($_POST['name']);$i++)
+	{
+	$propertyArray['p_id'] = $_POST['propertyID'];
+    $propertyArray['title'] = $_POST['title'][$i];
+	$propertyArray['type'] = $_POST['name'][$i];
+	$propertyArray['size'] = $_POST['size'][$i];
+	$propertyArray['price'] = $_POST['price'][$i];
+	$propertyArray['carpet_area'] = $_POST['carpet_area'][$i];
+	$propertyArray['built_area'] = $_POST['built_area'][$i];
+    $result = $builderObj->addUnitsByValue($propertyArray);
+	}
 	if($result)
 	{	
 		$response['status'] = '1'; 
@@ -63,6 +166,33 @@ if($_REQUEST['action']=="addPropertieAddress")
 	echo json_encode($response);
 }
 
+
+
+if($_REQUEST['action']=="getWings")
+{
+    $result = $builderObj->getWingBypropertyId($_POST['propertyID']);
+
+	if($result)
+	{	
+		$response['status'] = '1'; 
+
+		$response['message'] = 'success';
+		$response['result'] = $result;
+
+	}
+
+	else
+
+	{
+
+		$response['status'] = '0'; 
+
+		$response['message'] = 'error';
+		$response['result'] = $result;
+
+	}
+	echo json_encode($response);
+}
 ?>
 
 
