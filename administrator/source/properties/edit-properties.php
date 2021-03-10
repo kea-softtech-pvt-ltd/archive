a2ad9f7e2e710c86b8136ae13fcd121b7eb45fa3
unknown <gauravp1@keasofftech.com>
2021-03-10 12:24:33 +0530

    some changes on that page

---
+++ administrator/source/properties/edit-properties.php
@@ -0,0 +1,95 @@
+<?php
+	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . '/includefiles.php');
+	include_once(realpath(dirname(dirname(dirname(dirname(__FILE__))))) . "/common/model/properties.php");
+
+	$builderObj = new Model_Property();
+
+	$builderuserArray = $builderObj->getUserNameByUserId($_REQUEST['id']);
+	$floorsArray = $builderObj->getFloorsByUserId($_REQUEST['id']);
+	$wingArray = $builderObj->getWingByUserId($_REQUEST['id']);
+
+	//error_reporting(0);
+
+	// print_r($floorsArray);
+	// die();
+
+	$smarty->assign('builderuserArray', $builderuserArray);
+	$smarty->assign('floorsArray', $floorsArray);
+	$smarty->assign('wingArray', $wingArray);
+	//print_r($_REQUEST);
+	
+	if(isset($_POST['propetyEdit'])) {
+
+		$targetDir = "../upload/properties/"; 
+			$imagearray = array();
+			foreach($_FILES['image']['name'] as $key=>$val){ 
+			   
+				$fileName =  time().'_'.basename($_FILES['image']['name'][$key]); 
+				$targetFilePath = $targetDir . $fileName; 
+				
+			   $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
+				if(move_uploaded_file($_FILES["image"]["tmp_name"][$key], $targetFilePath)){ 
+					array_push($imagearray,$fileName);
+				}
+			} 
+		   
+		$imagelist = implode(",",$imagearray);
+
+		$builderObj = new Model_Property();
+		$propertyArray['id'] = $_POST['id'];
+		$propertyArray['name'] = $_POST['name'];
+		$propertyArray['builder_name'] = $_POST['builder_name'];
+		$propertyArray['started_date'] = $_POST['started_date'];
+		$propertyArray['possession_date'] = $_POST['possession_date'];
+		$propertyArray['rera_number'] = $_POST['rera_number'];
+		$propertyArray['address'] = $_POST['address'];
+		$propertyArray['specification'] = $_POST['specification'];	
+		$propertyArray['images'] = $imagelist;
+		$propertyArray['status'] = 1;
+		$propertyArray['created_date'] = date('Y/m/d H:i:s');
+		$propertyArray['modified_date'] = date('Y/m/d H:i:s');
+		// $propertyArray['wing'] = implode(',',$_POST['wing']) ;
+		// $propertyArray['floor'] = implode(',',$_POST['floor']) ;
+
+		$propertyId = $builderObj->editUserValueById($propertyArray,$_POST['id']);
+		// echo '<pre>';
+		// print_r($_POST);die;
+		for($i=0; $i<count($_POST['wing']);$i++){
+
+			$wingArray['p_id'] 		= $_POST['id'];
+			$wingArray['wing'] 		= $_POST['wing'][$i];
+			$wingArray['floor'] 	= $_POST['floor'][$i];
+			$wingArray['flat'] 		= $_POST['flat'][$i];
+			$wingArray['specality'] = $_POST['specality'][$i];
+			// print_r($wingArray);
+			// $builderObj->addFloorsByValue($wingArray);
+			if(empty($_POST['f_id'][$i])){
+				$builderObj->addFloorsByValue($wingArray);
+			}else{
+				$propertyId = $builderObj->editFloorValueById($wingArray,$_POST['f_id'][$i]);
+			}
+			
+		}
+		for($i=0; $i<count($_POST['wing']);$i++){
+
+			$wingsArray['p_id'] 		= $_POST['id'];
+			$wingsArray['name'] 		= $_POST['wings'][$i];
+
+			
+			if(empty($_POST['w_id'][$i])){
+				$builderObj->addWingsByValue($wingsArray);
+			}else{
+				$propertyId = $builderObj->editWingValueById($wingsArray,$_POST['w_id'][$i]);
+			}
+			
+		}
+
+		header("Location: " . SITE_URL . "/admin/properties");
+	}
+	$statesListArray = $builderObj->getWing();
+	$smarty->assign('statesListArray', $statesListArray);
+
+	$smarty->assign('moduleName', 'Add properties');
+	$smarty->display(ADMIN_TEMPLATEDIR . '/properties/edit-properties.tpl');
+	
+?>
